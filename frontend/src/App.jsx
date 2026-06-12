efd1eb48b5eb21e37bb39eaeabb19ab953bcd2e5
ricardoruizmocholi <ricardoruizm99@gmail.com>
2026-06-12 10:27:28 +0200

    feat: add card component with props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header'
+import Card from './components/Card'
 
 function App() {
   return (
     <div>
       <Header />
+      <Card title="React" description="Frontend del universo" />
+      <Card title="Laravel" description="Backend del universo" />
+      <Card title="Docker" description="Contenedor del universo" />
     </div>
   )
 }
